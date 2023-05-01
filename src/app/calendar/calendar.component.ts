29ffb74e4fd08e6781df53c2543d464962267f66
Andrey Sazonov <fromlhz03@gmail.com>
2023-05-01 19:47:51 +0300

    create main calendar component

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalendarHeaderComponent } from './components/calendar-header/calendar-header.component';
+import { BurgerComponent } from '../shared/components/burger/burger.component';
+import { DatePictureComponent } from '../shared/components/date-picture/date-picture.component';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CalendarHeaderComponent,
+    BurgerComponent,
+    DatePictureComponent,
+  ],
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalendarComponent {}
+export class CalendarComponent {
+  currentDate: number = new Date().getDate();
+}
